7a2681b8cbc306985c1925c8f7cda89a30afed40
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2013-10-23 22:47:40 +0100

    Switched gcalctool with gnome-calculator

---
+++ gnome-calculator/autogen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="gnome-calculator"
+REQUIRED_AUTOMAKE_VERSION=1.7
+REQUIRED_YELP_TOOLS_VERSION=3.1.1
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
